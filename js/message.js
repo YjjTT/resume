472fa48b683f373dd707ea66c6aed19848e342cd
YjjTT <yjjtt@tempot.com>
2019-03-20 12:52:10 +0800

    update message

--- js/message.js
+++ js/message.js
@@ -5,28 +5,35 @@ AV.init({
     appKey: APP_KEY
 });
 var query = new AV.Query('Message');
-  query.find().then(function (messages) {
-      let array = messages.map((item)=> item.attributes )
-      array.forEach((item)=>{
-          let li = document.createElement('li')
-          li.innerText = item.content
-          let messageList = document.querySelector('#messageList')
-          messageList.appendChild(li)
-      })
-  }, function (error) {
-      alert('提交失败')
-  }).then(()=>{},(error)=>{
-      console.log(error)
-  });
+query.find().then(function (messages) {
+    let array = messages.map((item) => item.attributes)
+    array.forEach((item) => {
+        let li = document.createElement('li')
+        li.innerText = `${item.name}: ${item.content}`
+        let messageList = document.querySelector('#messageList')
+        messageList.appendChild(li)
+    })
+}, function (error) {
+    alert('提交失败')
+}).then(() => {}, (error) => {
+    console.log(error)
+});
 let myForm = document.querySelector('#postMessageForm')
 myForm.addEventListener('submit', function (e) {
     e.preventDefault()
     let content = myForm.querySelector('input[name=content]').value
+    let name = myForm.querySelector('input[name=name]').value
     var Message = AV.Object.extend('Message');
     var message = new Message();
     message.save({
+        name: name,
         content: content
     }).then(function (object) {
-        window.location.reload()
+        let li = document.createElement('li')
+        li.innerText = `${object.attributes.name}: ${object.attributes.content}`
+        let messageList = document.querySelector('#messageList')
+        messageList.appendChild(li)
+        myForm.querySelector('input[name=content]').value = ''
+        
     })
 })
